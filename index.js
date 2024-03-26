1878ed101730d08fabc842316e881fe0b73e80d7
Reniel George Mababa <mabsrencode@gmail.com>
2024-03-26 14:16:28 +0800

    origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     // preflightContinue: false,
     // optionsSuccessStatus: 204,
